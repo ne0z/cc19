ab7d07b1ea8d3fdd823af453de805def405326cb
Vincenzo Maffione <v.maffione@gmail.com>
2019-03-29 18:48:01 +0100

    be quick or be dead 2

--- reverse/frida/solver-be-quick-or-be-dead-2.py
+++ reverse/frida/solver-be-quick-or-be-dead-2.py
@@ -8,11 +8,16 @@ pid = frida.spawn("./be-quick-or-be-dead-2")
 print("pid =", pid)
 session = frida.attach(pid)
 
+# Replace the "calculate_key()" function with a function that directly
+# returns Fibonacci(1026).
+# We can use an online tool to get the 1026-th Fibonacci's number,
+# and then use Python to compute the modulo 2^32:
+# >>> 11798692818055232550147578884125865608089028544560913468519228968187430794620907976123201977895385245239705082830656904630178314159866370495211539023461052682811230321796555930907722724384131648527339458407317543768 % pow(2,32)
 script = session.create_script("""
 'use strict';
-Interceptor.replace(ptr('0x40074b'), new NativeCallback(function() {
+Interceptor.replace(ptr('0x40074b'), new NativeCallback(function(n) {
 	return 0xf70a9b58;
-},'int32', ['int32']));
+},'int64', ['int64']));
 
 """)
 
